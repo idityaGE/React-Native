170b960b44637d0780321daeeff8a01ac81882de
idityaGE <am44910606@gmail.com>
2025-02-07 00:33:41 +0530

    Update app configuration: rename app and slug, adjust font family settings, and refine color definitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // NOTE: Update this to include the paths to all of your component files.
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
@@ -8,10 +7,10 @@ module.exports = {
       fontFamily: {
         rubik: ["Rubik-Regular", "sans-serif"],
         "rubik-bold": ["Rubik-Bold", "sans-serif"],
-        "rubik-light": ["Rubik-Light", "sans-serif"],
+        "rubik-extrabold": ["Rubik-ExtraBold", "sans-serif"],
         "rubik-medium": ["Rubik-Medium", "sans-serif"],
         "rubik-semibold": ["Rubik-SemiBold", "sans-serif"],
-        "rubik-extrabold": ["Rubik-ExtraBold", "sans-serif"],
+        "rubik-light": ["Rubik-Light", "sans-serif"],
       },
       colors: {
         primary: {
@@ -22,15 +21,15 @@ module.exports = {
         accent: {
           100: "#FBFBFD",
         },
-        black : {
+        black: {
           DEFAULT: "#000000",
           100: "#8C8E98",
           200: "#666876",
-          300: "#191d31",
+          300: "#191D31",
         },
-        danger : "#F75555",
+        danger: "#F75555",
       },
     },
-    plugins: [],
-  }
-}
+  },
+  plugins: [],
+};
